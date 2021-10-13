302b4c93ae2651c6af375a8c9674315fa57a8883
Richard Lopez <richardlopez@dyn-160-39-11-138.dyn.columbia.edu>
2021-10-13 16:36:09 -0400

    finished BFS and works :)

--- Graphs.py
+++ Graphs.py
@@ -11,17 +11,35 @@ graph = {
     "G": ["E"]
 }
 
+def bfs( root, graph ): # Pass in A, for example
+    queue = []
+    queue.append(root)
+    visited = set()
+    ordered_visit = []
+    while queue: 
+        curr_node = queue.pop(0)
+        visited.add(curr_node)
 
-def bfs( root ): 
-    pass
+        # this is just to keep track of the 
+        # order of the visit of each node. 
+        # still need to make sure node wasn't visited
+        if curr_node not in ordered_visit: 
+            ordered_visit.append(curr_node)
+        
+        for neighbor in graph[curr_node]: 
+
+            if neighbor not in visited: 
+
+                queue.append(neighbor)
+            
+    return " ".join( ordered_visit )
+
+
 
 def dfs( root ): 
     pass
 
 
 if __name__ == '__main__': 
-    pass
-
-
-
+    print( "Visited graph this order: ", bfs("A", graph) )
 
