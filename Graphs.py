956d847b04ddf228d269830622f0ded417ff5a36
Richard Lopez <richardlopez@dyn-160-39-11-138.dyn.columbia.edu>
2021-10-13 16:22:16 -0400

    Created graph and about to write search algos

---
+++ Graphs.py
@@ -0,0 +1,27 @@
+
+# graph implementation using adjacency list
+
+graph = {
+    "A": ["B", "D"], 
+    "B": ["C", "F"], 
+    "C": ["G", "E"],
+    "D": ["F"],
+    "E": ["B", "F"],
+    "F": ["A"],
+    "G": ["E"]
+}
+
+
+def bfs( root ): 
+    pass
+
+def dfs( root ): 
+    pass
+
+
+if __name__ == '__main__': 
+    pass
+
+
+
+
