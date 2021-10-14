c825b0e4fd42ef26fc4e5e93959d2f98439e6889
Richard Lopez <richardlopez@dyn-160-39-11-138.dyn.columbia.edu>
2021-10-14 16:45:07 -0400

    completed best time to buy and sell a stock

---
+++ leetcode-questions/bestTimeToBuySell.py
@@ -0,0 +1,25 @@
+def maxProfit(self, prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    
+    if len(prices) <= 1: 
+        return 0
+    
+    buy = prices[0]
+    max_profit = 0
+    
+    for i in range(1, len(prices)): 
+        
+        if buy > prices[i]: 
+            buy = prices[i]
+            
+        elif prices[i] - buy > max_profit:
+            max_profit = prices[i] - buy
+            
+    return max_profit
+
+
+if __name__ == '__main__': 
+    print( maxProfit([7,6,8,10,12]) )
